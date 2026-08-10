0078adc0aa58674447099fcaf3a2aa5bea68d504
25DCE021-Anandi <dihoraanandi@gmail.com>
2026-08-10 19:04:47 +0530

    Add files via upload

--- Practical-4/script.js
+++ Practical-4/script.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 document.querySelector("form").addEventListener("submit", function(event)
 {
     event.preventDefault();
@@ -14,4 +15,22 @@ document.querySelector("form").addEventListener("submit", function(event)
     {
         alert("Wrong Username or Password");
     }
+
+document.querySelector("form").addEventListener("submit", function(event)
+{
+    event.preventDefault();
+
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+
+    if(username == "Anandi" && password == "1234")
+    {
+        alert("Login Successful");
+        window.location.href = "index.html";
+    }
+    else
+    {
+        alert("Wrong Username or Password");
+    }
+>>>>>>> 0b25317 (UPdate files)
 });
