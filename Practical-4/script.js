d059eb61e382710918244c0140515b78b8286fcf
AD-9218 <anandidihora@gmail.com>
2026-08-11 21:38:29 +0530

    Update Practical-4 login script

--- Practical-4/script.js
+++ Practical-4/script.js
@@ -1,36 +1,13 @@
-<<<<<<< HEAD
-document.querySelector("form").addEventListener("submit", function(event)
-{
+document.querySelector("form").addEventListener("submit", function(event) {
     event.preventDefault();
 
     let username = document.getElementById("username").value;
     let password = document.getElementById("password").value;
 
-    if(username == "Anandi" && password == "1234")
-    {
+    if (username === "Anandi" && password === "1234") {
         alert("Login Successful");
         window.location.href = "index.html";
-    }
-    else
-    {
+    } else {
         alert("Wrong Username or Password");
     }
-
-document.querySelector("form").addEventListener("submit", function(event)
-{
-    event.preventDefault();
-
-    let username = document.getElementById("username").value;
-    let password = document.getElementById("password").value;
-
-    if(username == "Anandi" && password == "1234")
-    {
-        alert("Login Successful");
-        window.location.href = "index.html";
-    }
-    else
-    {
-        alert("Wrong Username or Password");
-    }
->>>>>>> 0b25317 (UPdate files)
 });
